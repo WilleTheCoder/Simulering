fc6a9d5f92723a07442b47492f7f3eabc7bd3a9e
willethecoder <wille-hif@hotmail.com>
2023-04-13 19:28:36 +0200

    task4 started

---
+++ Assignment 1/Task4/QS.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+// This class defines a simple queuing system with one server. It inherits Proc so that we can use time and the
+// signal names without dot notation
+class QS extends Proc{
+	public int numberInQueue = 0, numberInQueueN = 0, numberInQueueS = 0,  totalNumberInQueueS = 0, accumulated, noMeasurements;
+	public Proc sendTo;
+	Random slump = new Random();
+
+	public double a = 300, x1 = 240, percentage = 0.1;
+
+	ArrayList<Double> timeListNStart = new ArrayList<Double>();
+	ArrayList<Double> timeListNEnd = new ArrayList<Double>();
+	ArrayList<Double> timeListSStart = new ArrayList<Double>();
+	ArrayList<Double> timeListSEnd = new ArrayList<Double>();
+
+	public void TreatSignal(Signal x){
+		switch (x.signalType){
+			
+			case ARRIVAL:{
+				
+				if (this.isSkipper(percentage)){
+					numberInQueueS++;
+					timeListSStart.add(time);
+				} else{
+					numberInQueueN++;
+					timeListNStart.add(time);
+				}
+
+				if (numberInQueue == 0){
+					SignalList.SendSignal(READY,this, time + exp(x1));
+				}
+				numberInQueue++; 
+			} break;
+
+			case READY:{
+
+				if (sendTo != null){
+					SignalList.SendSignal(ARRIVAL, sendTo, time);
+				}
+
+				if (numberInQueue > 0){
+					numberInQueue--;
+
+					if(numberInQueueS > 0){
+						numberInQueueS--;
+						timeListSEnd.add(time);
+					} else{
+						numberInQueueN--;
+						timeListNEnd.add(time);
+					}
+
+					SignalList.SendSignal(READY, this, time + exp(x1));
+				}
+			} break;
+
+			case MEASURE:{
+				noMeasurements++;
+				accumulated = accumulated + numberInQueue;
+				SignalList.SendSignal(MEASURE, this, time + 2*slump.nextDouble());
+			} break;
+		}
+	}
+
+	public double exp(double time){
+		double lambda = 1/time;
+		return Math.log(1-slump.nextDouble())/(-lambda);
+	}
+
+	private boolean isSkipper(double percentage){
+		int chance =  (int)(percentage*10);
+		return slump.nextInt(10) < chance;
+	}
+
+
+}
