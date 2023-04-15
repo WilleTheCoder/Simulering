e4d861fcf2cb7b4c36c0a8f145a86ca07d636de3
willethecoder <william.mauritzon.3328@student.lu.se>
2023-04-15 18:19:47 +0200

    updated repo

--- Assignment1/Task6/MainSimulation.java
+++ Assignment1/Task6/MainSimulation.java
@@ -8,11 +8,12 @@ public class MainSimulation extends GlobalSimulation{
     	Event actEvent;
     	State actState = new State(); // The state that shoud be used
 		double totTime = 0;
+		int arrivals = 1000;
     	// Some events must be put in the event list at the beginning
         // insertEvent(ARRIVALS, 0);  
         
 		// 1000 runs of the system
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < arrivals; i++) {
 				insertEvent(ARRIVALS, 0);
 				// insertEvent(MEASURE, 5);
 	
@@ -23,16 +24,18 @@ public class MainSimulation extends GlobalSimulation{
 					actState.treatEvent(actEvent);
 				}
 	
-				totTime += GlobalSimulation.time;
-				GlobalSimulation.time = 0;
+				//reset it
 				GlobalSimulation.eventList = new EventListClass();
-				actEvent = null;
+				totTime += time;
+				GlobalSimulation.time = 0;
 				actState = new State();
+				actEvent = null;
 			}
 
-		System.out.println(totTime/1000);
-    	
-    	// Printing the result of the simulation, in this case a mean value
-    	// System.out.println(1.0*actState.accumulated/actState.noMeasurements);
+		System.out.println("--------TASK4--------\n");
+			
+		System.out.println(totTime/arrivals);
+
+		System.out.println("\n----------END----------");
     }
 }
