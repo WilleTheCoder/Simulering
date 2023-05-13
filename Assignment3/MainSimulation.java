d0502c389699b38c1949c0ea31ffe8f5cc8e4231
willethecoder <william.mauritzon.3328@student.lu.se>
2023-05-13 15:45:15 +0200

    updated a3

--- Assignment3/MainSimulation.java
+++ Assignment3/MainSimulation.java
@@ -4,77 +4,99 @@ import java.io.*;
 //Denna klass �rver Global s� att man kan anv�nda time och signalnamnen utan punktnotation
 //It inherits Proc so that we can use time and the signal names without dot notation
 
+public class MainSimulation extends Global {
 
-public class MainSimulation extends Global{
+	public static Config config;
+	public static FileWriter fw;
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		int m = 200;
+		fw = new FileWriter("res1.txt");
 
-    	//Signallistan startas och actSignal deklareras. actSignal �r den senast utplockade signalen i huvudloopen nedan.
-    	// The signal list is started and actSignal is declaree. actSignal is the latest signal that has been fetched from the 
-		// signal list in the main loop below.
-
-		FileWriter fw = new FileWriter("res.txt");
-		Config config = Config.get_config();
+		config = Config.get_config();
 		config.load_config();
+		int nl = config.n_arr_i.length;
 
-		for (int n : config.n_arr_i) {
+		List<Double> loss_rates = new ArrayList<>();
+
+		boolean overlap = true;
+
+		Double intervals[][] = new Double[m][3];
+
+		while (overlap) {
+			int idx = 0;
+			for (int n : config.n_arr_i) {
+				for (int i = 0; i < m; i++) {
+					double loss_rate = run(n);
+					loss_rates.add(loss_rate);
+				}
+				System.out.println("Running with Network Load: " + n);
+				double[] inter = confidenceInterval(loss_rates);
+				intervals[idx][0] = inter[0];
+				intervals[idx][1] = inter[1];
+				idx++;
+				System.out.println("conf for run " + idx + ": " + inter[0] + " " + inter[1]);
+			}
+			overlap = false;
+			for (int i = 0; i < nl; i++) {
+				for (int j = i + 1; j < nl; j++) {
+					if (intervals[i][1] > intervals[j][0] && intervals[j][1] > intervals[i][0]) {
+						overlap = true;
+						break;
+					}
+				}
+			}
+
+			
+		}
+		
+		for (int nx : config.n_arr_i) {
+			fw.write(nx + " " + intervals[nx][3] + "\n");
+		}
+		fw.close();
+	}
+
+	private static double run(int n) throws IOException {
+
 		time = 0;
 		Gateway gateway = new Gateway();
-		System.out.println("Running with Network Load: "+ n);
-
-    	Signal actSignal = null;
-    	new SignalList();
-
-    	//H�r nedan skapas de processinstanser som beh�vs och parametrar i dem ges v�rden.
-    	// Here process instances are created (two queues and one generator) and their parameters are given values. 
-
-		Random slump = new Random();
+		Signal actSignal = null;
+		new SignalList();
 
 		Sensor[] sensors = new Sensor[n];
 
-		for (int i = 0; i < sensors.length; i++) {
-			sensors[i] = new Sensor();
-			sensors[i].gateway = gateway;
+		for (int j = 0; j < sensors.length; j++) {
+			sensors[j] = new Sensor();
+			sensors[j].gateway = gateway;
 		}
-		
+
 		gateway.sensors = sensors;
+		SignalList.SendSignal(START, gateway, time, null);
 
-    	//H�r nedan skickas de f�rsta signalerna f�r att simuleringen ska komma ig�ng.
-    	//To start the simulation the first signals are put in the signal list
+		while (time < 1000) {
+			actSignal = SignalList.FetchSignal();
+			time = actSignal.arrivalTime;
+			actSignal.destination.TreatSignal(actSignal);
+		}
 
-    	SignalList.SendSignal(START, gateway, time, null);
-    	// SignalList.SendSignal(MEASURE, Q1, time);
+		// ---------------A---------------
 
-    	// Detta �r simuleringsloopen:
-    	// This is the main loop
+		// double lambda = gateway.no_transmissions/time;
+		// double throughput= lambda * config.tp * Math.exp(-2 * lambda * config.tp);
+		// System.out.println("Throughput: " + throughput);
+		// fw.write(n + " " + throughput + "\n");
 
-    	while (time < 100000) {
-    		actSignal = SignalList.FetchSignal();
-    		time = actSignal.arrivalTime;
-    		actSignal.destination.TreatSignal(actSignal);
-    	}
-		//Slutligen skrivs resultatet av simuleringen ut nedan:
-		//Finally the result of the simulation is printed below:
-		System.out.println("-.-");
-		System.out.println("Number of transmissions: "+ gateway.no_transmissions);   
-		System.out.println("Number of collisions: "+ gateway.no_collisions);    
-		System.out.println("Number of successfull transmissions: "+ gateway.no_success);   
-		double lambda = gateway.no_transmissions/time;
-		double throughput2 = lambda * config.tp * Math.exp(-2 * lambda * config.tp);
-
-		double throughput  = gateway.no_success/time;
-		System.out.println("Number of successful transmissions per unit of time: " + throughput);
-		fw.write(n + " " + throughput2 + "\n");
-
-		System.out.println("----------------------------------------");
+		// ---------------B---------------
+		double loss_rate = (double) gateway.no_collisions / gateway.no_transmissions;
+		return loss_rate;
 	}
-	
-	fw.close();
 
-
-		// fw.write();
-
-		// double x = (double) gateway.no_success / gateway.no_transmissions;
-		// System.out.println(x);
+	public static double[] confidenceInterval(List<Double> list) {
+		double mean = list.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+		double standardDeviation = Math
+				.sqrt(list.stream().mapToDouble(num -> Math.pow(num - mean, 2)).average().orElse(0.0));
+		double confidenceLevel = 1.96;
+		double temp = confidenceLevel * standardDeviation / Math.sqrt(list.size());
+		return new double[] { mean - temp, mean + temp, mean };
 	}
 }
